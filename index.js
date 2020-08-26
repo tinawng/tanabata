a7285622164d83518c6e73074a57dca91dbd30bf
Tina Wang <tinawang@laptop>
2020-08-26 16:58:41 +0200

    🐛 Fix CORS rejections

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 import fastify from 'fastify';
-import mongoose from 'mongoose'
+import cors from 'fastify-cors'
 import jwt from 'jsonwebtoken';
 
 dotenv.config();
@@ -19,13 +19,13 @@ app.listen(8082, '0.0.0.0', (err) => {
 //   done()
 // })
 
-
+// Add cors header
+app.register(cors, { origin: '*' });
 
 // Decorate request with a 'user' property
 app.decorateRequest('is_auth', '')
-
 // Update our property
-app.addHook('preHandler', (req, res, done) => {
+app.addHook('preHandler', (req, reply, done) => {
   req.is_auth = false;
 
   if (req.headers.authorization)
@@ -35,17 +35,26 @@ app.addHook('preHandler', (req, res, done) => {
       req.is_auth = true;
       req.user_id = decoded.user_id;
     } catch (error) {
-      res.code(401).send({ message: "Invalid Token. 💔 Please, signin again! " });
+      reply.code(401).send({ message: "Invalid Token 💔" });
     }
 
   done()
 })
-// And finally access it
+
 app.get('/', (req, reply) => {
   if (req.is_auth)
     reply.send("You're in 🔓 \nHi there 👋 " + req.user_id)
   else
     reply.send("You're out 🔒")
+})
+app.post('/is_token_valid', (req, reply) => {
+  try {
+    const token = req.body.token;
+    jwt.verify(token, process.env.SECRET);
+    reply.code(200).send(true);
+  } catch (error) {
+    reply.code(200).send(false);
+  }
 })
 
 // app.get('/', function (req, res) {
@@ -57,6 +66,5 @@ app.register(root_cmd, { prefix: "/cmd" });
 import root_mongo from './roots/mongo/index.js';
 app.register(root_mongo, { prefix: "/mongo" });
 import root_records from './roots/records/index.js';
-import Review from './roots/records/models/Review.js';
 app.register(root_records, { prefix: "/records" });
 
