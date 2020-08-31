37442b090e1af8754ec725b8e43505f5efb8abc1
Tina Wang <tinawang@laptop>
2020-08-31 16:14:14 +0200

    🚨 Fix deprecated warnings

--- mongo.js
+++ mongo.js
@@ -4,9 +4,12 @@ import dotenv from 'dotenv';
 dotenv.config(); // preload env vars
 
 import mongoose from 'mongoose';
-mongoose.set('useCreateIndex', true); // deprecation warning otherwise
+mongoose.set('useNewUrlParser', true);      // deprecation warning
+mongoose.set('useUnifiedTopology', true);   // deprecation warning
+mongoose.set('useCreateIndex', true);       // deprecation warning
+mongoose.set('useFindAndModify', false);    // deprecation warning
 
-const db_garden = mongoose.createConnection('mongodb://' + process.env.MONGODB_HOST + '/garden', { useNewUrlParser: true, useUnifiedTopology: true });
-const db_records = mongoose.createConnection('mongodb://' + process.env.MONGODB_HOST + '/records', { useNewUrlParser: true, useUnifiedTopology: true });
+const db_garden = mongoose.createConnection('mongodb://' + process.env.MONGODB_HOST + '/garden');
+const db_records = mongoose.createConnection('mongodb://' + process.env.MONGODB_HOST + '/records');
 
 export { db_garden, db_records };
